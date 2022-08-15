02b20603c188f7f6cfa48cf2cead81ac3f60ead6
caseyjramirez <caseyjramirez5@gmail.com>
2022-08-15 17:32:41 -0500

    commit tree 5c787019e7dd6c7ea693c018ecf59baa4678df1c

--- index.js
+++ index.js
@@ -1,25 +1,61 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let isSame = true
+  const forwards = []
+  const backwards = []
+
+  for (let letter of word) {
+    forwards.push(letter)
+    backwards.unshift(letter)
+  }
+  
+  for (let i = 0; i < forwards.length; i++) {
+    if(forwards[i] !== backwards[i]) {isSame = false}
+  }
+
+  return isSame
 }
+
+
+/* 
+  Rewrite the problem in your own words
+  
+  when given a string, the Palidrome function will reverse the sequence of letters in that string and see if it is the same as the original input
+*/
+
+
+
 
 /* 
   Add your pseudocode here
+
+  function that recieves a string
+  two arrays are created one for the letters in normal fashion
+  the other for the letters backwards
+  then there is a for.. of loop iterates over each letter of that string and pushes/unshifts to the respective array
+  Each element of the two arrays are compared against one another
+  if they are the same return true
+  else return false
 */
 
 /*
   Add written explanation of your solution here
+  
+  to begin a flag called isSame was initialized with the value of true
+  
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log(isPalindrome("car"));
+  console.log("Expecting: true");
+  console.log(isPalindrome("bob"));
+
+  // console.log("");
+
+  // console.log("Expecting: false");
+  // console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
